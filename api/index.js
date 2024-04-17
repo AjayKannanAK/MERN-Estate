6355a5d2668e3a7601cfecdb7492894172655853
Ajay Kannan A K <ajaykannanak23@gmail.com>
2024-04-17 14:03:28 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,12 +6,15 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js'
+import path from 'path';
 
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("Connected to MongoDB!");
 }).catch((error) => {
     console.log(err);
 })
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -20,6 +23,12 @@ app.use(cookieParser());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.listen(8080, ()=> {
     console.log("Server is running on port 8080")
